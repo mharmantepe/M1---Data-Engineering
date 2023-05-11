f60c9a8c7808feafd4d944891d5634f9e93c05da
mharmantepe <sila.melis@gmail.com>
2023-05-12 00:00:47 +0200

    alert and storage files

---
+++ src/main/scala/client/Alerts.scala
@@ -0,0 +1,48 @@
+package client
+
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.SparkSession
+import spark.implicits._
+
+object Alerts {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder()
+      .appName("Alerts")
+      .master("local[2]")
+      .getOrCreate()
+
+    // Define a streaming source
+    val source = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "my-topic")
+      .load()
+
+    // Define a streaming query
+    val query = source
+      .selectExpr("CAST(value AS STRING)")
+      .as[String]
+      .flatMap(_.split(" "))
+      .groupBy($"value")
+      .count()
+      .filter($"count" > 100)
+
+    // Define alert conditions
+    val alertQuery = query.writeStream
+      .outputMode("complete")
+      .foreachBatch { (batchDF, batchId) =>
+        val count = batchDF.first().getAs[Long]("count")
+        if (count > 1000) {
+          sendAlert(s"Alert! Count exceeded threshold: $count")
+        }
+      }
+      .start()
+
+    alertQuery.awaitTermination()
+  }
+
+  def sendAlert(message: String): Unit = {
+    // Send alert using appropriate channels, such as email, Slack, or PagerDuty.
+    println(message)
+  }
+}
